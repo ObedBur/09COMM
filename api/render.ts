bd3d6822c0ea406fc09fff1966f36e0b8cf2cdc5
Obed <obedburindi@gmail.com>
2026-04-04 13:42:41 +0200

    fix: update SSR handler signature for Angular 21

--- api/render.ts
+++ api/render.ts
@@ -1,14 +1,18 @@
 import {
   AngularNodeAppEngine,
   createNodeRequestHandler,
-  writeResponseToNodeResponse,
 } from '@angular/ssr/node';
-import { Request, Response } from 'express';
-import { join } from 'node:path';
+import { Request, Response, NextFunction } from 'express';
 
 const angularApp = new AngularNodeAppEngine();
 
-export default async function handler(req: Request, res: Response) {
-  const result = await (createNodeRequestHandler(angularApp)(req, res));
-  return result;
+export default async function handler(req: Request, res: Response, next: NextFunction) {
+  try {
+    // On passe 'next' pour satisfaire les 3 arguments attendus (TS2554)
+    // createNodeRequestHandler retourne une fonction qui prend (req, res, next)
+    await createNodeRequestHandler(angularApp)(req, res, next);
+  } catch (err) {
+    // Gestion d'erreur si le rendu échoue
+    next(err);
+  }
 }
